74d99c7a69f8c23881520edf054e4d87c9e09873
VidGantar <gantar.vid@gmail.com>
2026-04-06 20:55:19 +0200

    redirect

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,11 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+// Handle redirect from 404.html for GitHub Pages SPA routing
+if (sessionStorage.redirect) {
+  const redirect = sessionStorage.redirect;
+  delete sessionStorage.redirect;
+  window.location.href = redirect;
+}
+
 createRoot(document.getElementById("root")!).render(<App />);
